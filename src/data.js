fc5dcf5498ddcf33efafb93aea919e516f905095
Karthik <33841263+codeincrypt@users.noreply.github.com>
2021-07-26 19:14:07 +0530

    search v1

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const alldatalist = [
+  {
+    id:"B004",
+    name:"kartik",
+    item:"555",
+    address:"Purnea city",
+    pincode:"854301"
+  },
+  {
+    id:"B005",
+    name:"Aman Kumar",
+    item:"456",
+    address:"Pune, Maharashtra",
+    pincode:"400102"
+  },
+  {
+    id:"B005678658756",
+    name:"Gautam Kumar",
+    item:"458",
+    address:"Delhi, New Delhi",
+    pincode:"100012"
+  },
+  {
+    id:"B57647657007",
+    name:"John Doe",
+    item:"666",
+    address:"Bangalore",
+    pincode:"700102"
+  },
+  {
+    id:"B00777",
+    name:"Anamika Kumar",
+    item:"458",
+    address:"Delhi, New Delhi",
+    pincode:"100012"
+  },
+  {
+    id:"B00456567",
+    name:"John Doe",
+    item:"666",
+    address:"Bangalore",
+    pincode:"700102"
+  }
+];
+
+module.exports = alldatalist
